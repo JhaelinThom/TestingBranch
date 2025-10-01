3b490303cfe35036f9ba79dd58b2b1dd60f770bd
IcyGalaxies <jhaelinthom@gmail.com>
2025-10-01 12:58:18 -0400

    Adding a new branch

--- userBranch.java
+++ userBranch.java
@@ -4,6 +4,8 @@ public class userBranch {
     for (int i = 0; i < 5; i++) {
         System.out.println("i = " + i);
         System.out.println("Loooooooops");
+
     }
+    System.out.println("This is the end of the loop!");
 }
 }
