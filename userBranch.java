c54b823f57563dadda91dc9ccf207202c5546934
IcyGalaxies <jhaelinthom@gmail.com>
2025-10-01 12:55:10 -0400

    Added a loop!

--- userBranch.java
+++ userBranch.java
@@ -1,5 +1,9 @@
 public class userBranch {
     public static void main(String[] args) {
     System.out.println("This shoud be visible if you have cloned properly!");
+    for (int i = 0; i < 5; i++) {
+        System.out.println("i = " + i);
+        System.out.println("Loooooooops");
+    }
 }
 }
